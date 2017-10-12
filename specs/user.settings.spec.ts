46913093f547a923fb71b1f6c18540cf9d8b7371
Simone B <simone.borelli@live.com>
2017-10-12 23:17:24 +0300

    Update user.settings.spec.ts

--- specs/user.settings.spec.ts
+++ specs/user.settings.spec.ts
@@ -3,50 +3,43 @@ import { LoginPage } from '../page_objects/login.page'
 import { UserSettings } from '../page_objects/user.settings.page'
 import { AppPage } from '../page_objects/app.page'
 
-declare let expect:any
 
 describe('Test editing of user settings', function () {
+    const userSettings = new UserSettings()
     
     beforeEach(async () => {
-        await new LoginPage().open()
-
         // new login for each test case
-        let loginPage = new LoginPage()
-        await loginPage.open()
-        await loginPage.loginAs({
+        new LoginPage().openAndLogin({
             email: 'simonex@sharklasers.com',
             password: 'sendIT'
-            })
-
+        })
         await new AppPage().openUserProfile()
     })
 
     it('should change the name', async function () {
-        let d = new Date()
-        let n = d.getTime().toString()
-        let userSettingInstance = new UserSettings()
-        await userSettingInstance.editName(n)
+        const epochTime = new Date().getTime().toString()
+        
+        await userSettings.editName(epochTime)
         await browser.sleep(300)
         await browser.refresh()
-        await expect($('editable-field[field-value="data.fullName"] input').getAttribute('value')).toEqual(n)
+        await expect(await $('editable-field[field-value="data.fullName"] input').getAttribute('value')).toEqual(n)
     })
 
 
     it('should change the job title', async function () {
-        let d = new Date()
-        let n = d.getTime().toString()
-        let userSettingInstance = new UserSettings()
-        await userSettingInstance.editJobTitle(n)
+        const epochTime = new Date().getTime().toString()
+        
+        await userSettings.editJobTitle(epochTime)
         await browser.sleep(300)
         await browser.refresh()
-        await expect($('editable-field[field-value="data.jobTitle"] input').getAttribute('value')).toEqual(n)
+        await expect(await $('editable-field[field-value="data.jobTitle"] input').getAttribute('value')).toEqual(n)
     })
 
     it('should change time format', async function () {
-        let userSettingInstance = new UserSettings()
-        let oldFormat = userSettingInstance.checkTimeFormat()
-        await userSettingInstance.editTimeFormat()
-        let newFormat = userSettingInstance.checkTimeFormat()
+        
+        const oldFormat = await userSettings.selectedTimeFormat()
+        await userSettings.switchTimeFormat()
+        const newFormat = await userSettings.selectedTimeFormat()
         await expect(oldFormat).not.toEqual(newFormat)
     })
 
