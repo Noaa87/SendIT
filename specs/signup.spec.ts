1f3b49f1834e49f0c4a85eae8267e25db56b8b98
Simone B <simone.borelli@live.com>
2017-10-12 23:09:09 +0300

    Update signup.spec.ts

--- specs/signup.spec.ts
+++ specs/signup.spec.ts
@@ -3,51 +3,41 @@ import { SignUpPage } from '../page_objects/signup.page'
 import { TermsAndConditionsPage } from '../page_objects/terms.conditions.page'
 import { AppPage } from '../page_objects/app.page'
 
-declare let expect:any
 
 describe('Test SendIT web-app', function () {
+    const signUpPage = new SignUpPage()
     beforeEach(async () => {
-        await new SignUpPage().open()
+        await signUpPage.open()
     })
 
     it('should get alert on invalid email submission', async function () {
-        let signUpPage = new SignUpPage()
-        await signUpPage.open()
         await signUpPage.signUpAs({
             fullname: "Some full name",
             email: 'invalid email',
             password: '123456'
-            }
-        )
-        await expect($('[ng-if="vm.error"]').getText()).toEqual('This is not a valid e-mail address. Please fix it and try again.') 
+        })
+        await expect(await $('[ng-if="vm.error"]').getText()).toEqual('This is not a valid e-mail address. Please fix it and try again.')
     })
 
     it('should get alert on invalid password submission (<6 chars)', async function () {
-        let signUpPage = new SignUpPage()
-        await signUpPage.open()
         await signUpPage.signUpAs({
             fullname: "Some full name",
             email: 'testemail@gmail.com',
             password: '12345'
-            }
-        )
-        await expect($('[ng-if="vm.error"]').getText()).toEqual('This password is invalid. Minimal length is 6 characters.') 
+        })
+        await expect(await $('[ng-if="vm.error"]').getText()).toEqual('This password is invalid. Minimal length is 6 characters.')
     })
 
     it('it should sign up with valid credentials, accept terms and logout', async function () {
-        let d = new Date()
-        let n = d.getTime();
-        let signUpPage = new SignUpPage()
-        await signUpPage.open()
+        let epochTime = new Date().getTime()
         await signUpPage.signUpAs({
             fullname: "Test Simone",
-            email: n+'@test.com',
+            email: `${epochTime}_test@test.com`,
             password: '123456'
-            }
-        )
+        })
         await new TermsAndConditionsPage().agreeTermsAndConditions()
         await new AppPage().LogOut()
-        await expect(browser.getCurrentUrl()).toContain('/login/')
+        await expect(await browser.getCurrentUrl()).toContain('/login/')
     })
 })
 
