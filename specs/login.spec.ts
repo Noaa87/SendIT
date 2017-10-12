d2f520bad44081c2b41b774a0fd9d2eccee469f0
Simone B <simone.borelli@live.com>
2017-10-12 23:06:36 +0300

    Update login.spec.ts

--- specs/login.spec.ts
+++ specs/login.spec.ts
@@ -1,56 +1,46 @@
 import { browser, ExpectedConditions as EC, $, $$ } from 'protractor'
 import { LoginPage } from '../page_objects/login.page'
 
-declare let expect:any
-
 describe('Test SendIT web-app', function () {
-    
+    const loginPage = new LoginPage()
+
     beforeEach(async () => {
-        await new LoginPage().open()
+        await loginPage.open()
     })
 
     it('should show alert on invalid email submission', async function () {
-        let loginPage = new LoginPage()
-        await loginPage.open()
         await loginPage.loginAs({
             email: 'invalid email',
             password: '123456'
-            })
-        await expect($('[ng-if="vm.error"]').getText()).toEqual('This is not a valid e-mail address. Please fix it and try again.') 
+        })
+        await expect(await $('[ng-if="vm.error"]').getText()).toEqual('This is not a valid e-mail address. Please fix it and try again.') 
     })
 
     it('should show alert on non registered email', async function () {
-        let d = new Date()
-        let n = d.getTime();
-        let loginPage = new LoginPage()
-        await loginPage.open()
+        let epochTime = new Date().getTime();
         await loginPage.loginAs({
-            email: n+'test@test.com',
+            email: `${epochTime}_test@test.com`,
             password: '123456'
             })
         await expect($('[ng-if="vm.error"]')).toAppear()
-        await expect($('[ng-if="vm.error"]').getText()).toEqual('Wrong e-mail or password. Try again please.')
+        await expect(await $('[ng-if="vm.error"]').getText()).toEqual('Wrong e-mail or password. Try again please.')
     })
 
     it('should get alert on invalid password submission (<6 chars)', async function () {
-        let loginPage = new LoginPage()
-        await loginPage.open()
         await loginPage.loginAs({
             email: 'testemail@gmail.com',
             password: '12345'
             }
         )
-        await expect($('[ng-if="vm.error"]').getText()).toEqual('This password is invalid. Minimal length is 6 characters.') 
+        await expect(await $('[ng-if="vm.error"]').getText()).toEqual('This password is invalid. Minimal length is 6 characters.') 
     })
 
     it('should login correctly with valid credentials', async function () {
-        let loginPage = new LoginPage()
-        await loginPage.open()
         await loginPage.loginAs({
             email: 'simonex@sharklasers.com',
             password: 'sendIT'
             })
-        await expect(browser.getCurrentUrl()).toContain('/app/')
+        await expect(await browser.getCurrentUrl()).toContain('/app/')
     })
 
 })
