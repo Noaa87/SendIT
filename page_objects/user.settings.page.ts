4a0949d0c9948182a3b0760082d5e8eb7662610c
Simone B <simone.borelli@live.com>
2017-10-12 23:17:51 +0300

    Update user.settings.page.ts

--- page_objects/user.settings.page.ts
+++ page_objects/user.settings.page.ts
@@ -13,37 +13,36 @@ export class UserSettings {
     async editJobTitle(newJobTitle: string) {
         await this.jobTitle.clear()
         await this.jobTitle.sendKeys(newJobTitle)
-        await this.name.click()
+        await this.jobTitle.submit()
     }
 
     async editName(newName: string) {
         await this.name.clear()
         await this.name.sendKeys(newName)
-        await this.jobTitle.click()
+        await this.name.submit()
     }
 
 
-    async selectTime(format: string) {
-        await this.options.map(async (option)=> {
-            let text = await option.getText()
-            if(text.includes(format)) {
-                 await option.click()
+    async switchTimeFormatTo(format: '24h' | '12h') {
+        await this.options.map(async (option) => {
+            const text = await option.getText()
+            if (text.includes(format)) {
+                await option.click()
             }
         })
     }
 
-    async checkTimeFormat () {
+    async selectedTimeFormat(): Promise<string> {
         return await this.timeFormat.$('span.value').getText()
     }
 
-    async editTimeFormat () {
+    async switchTimeFormat() {
         await this.timeFormat.click()
-        let options = $$('ul.dropdown-menu a')
-        let currentTime = await this.timeFormat.$('span.value').getText()
-        if (currentTime.includes('AM') || currentTime.includes('PM')) {
-            await this.selectTime('(24h)')
+        const currentTimeFormat = await this.selectedTimeFormat()
+        if (currentTimeFormat.includes('AM') || currentTimeFormat.includes('PM')) {
+            await this.switchTimeFormatTo('24h')
         } else {
-            await this.selectTime('(12h)')
+            await this.switchTimeFormatTo('12h')
         }
     }
 
